f557ed503c515dae99cd306b7fe696a0abe51fb1
itai gendler <itaigendler@itais-MBP.lan>
2022-12-14 17:29:39 +0200

    wip

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const { sendErrorResponse } = require("./server/error-handling/error-handler");
+const healthCheck = require("./src/healthcheck");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("combined"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(sendErrorResponse);
+
+async function main() {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+  healthCheck.main();
+}
+
+main().catch((err) => {
+  console.log(err);
+  console.log("exiting");
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error(err.stack);
+  process.exit(1);
+});
---
+++ src/healthcheck.js
@@ -0,0 +1,9 @@
+const livenessReport = () => {};
+
+const main = () => {
+  setInterval(livenessReport, 5000);
+};
+
+module.exports = {
+  main,
+};
