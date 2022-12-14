e6866a4f71a90d6c46d357202b935da69ec31b3e
kim-codefresh <kim.aharfi@codefresh.io>
2022-12-14 21:28:00 +0200

    wip

--- src/healthcheck.js
+++ src/healthcheck.js
@@ -1,17 +1,14 @@
 const axios = require("axios");
+const helper = require("./helper");
 
 const livenessReport = async () => {
-  // TODO upsert new agent
   const name = process.env.AGENT_NAME;
   const icon = process.env.AGENT_ICON;
   try {
-    const newAgent = await axios.put(
-      `http://localhost:3000/api/agent/${name}`,
-      {
-        name,
-        icon,
-      }
-    );
+    const newAgent = await axios.put(helper.getUrl(`/api/agent/${name}`), {
+      name,
+      icon,
+    });
     console.log(newAgent.data);
   } catch (error) {
     throw new Error(
