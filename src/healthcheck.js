465fd39558a984fc6aff470f8806a97ee78fe12e
kim-codefresh <kim.aharfi@codefresh.io>
2022-12-14 21:31:37 +0200

    remove redundent

--- src/healthcheck.js
+++ src/healthcheck.js
@@ -5,11 +5,10 @@ const livenessReport = async () => {
   const name = process.env.AGENT_NAME;
   const icon = process.env.AGENT_ICON;
   try {
-    const newAgent = await axios.put(helper.getUrl(`/api/agent/${name}`), {
+    await axios.put(helper.getUrl(`/api/agent/${name}`), {
       name,
       icon,
     });
-    console.log(newAgent.data);
   } catch (error) {
     throw new Error(
       `could not report liveness for agent ${name} caused by ${error}`
