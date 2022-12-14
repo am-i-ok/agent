f6329ffb79c0bd807ca2c8100fbe6e3e00775794
kim-codefresh <kim.aharfi@codefresh.io>
2022-12-14 20:27:03 +0200

    wip

--- src/healthcheck.js
+++ src/healthcheck.js
@@ -1,5 +1,18 @@
-const livenessReport = () => {};
+const axios = require("axios");
 
+const livenessReport = async (name, location) => {
+  // TODO upsert new agent
+  try {
+    await axios.put("../api/agent", {
+      name,
+      location,
+    });
+  } catch (error) {
+    throw new Error(
+      `could not report liveness for agent ${name} at ${location} caused by ${error}`
+    );
+  }
+};
 const main = () => {
   setInterval(livenessReport, 5000);
 };
